3063255e4ba814f88e5d888906dc86707b31a611
Gabriel Alucema <galucema@utem.cl>
2023-12-04 14:56:13 -0300

    modificado if a case para multiples condiciones

--- procedures.sql
+++ procedures.sql
@@ -14,50 +14,51 @@ AS &&
 begin
 
     --prueba 1
-    if p_direccion is null or trim(p_direccion) = '' then
-        insert into accion(
-            destinatario_accion,
-	        parametros,
-	        accion_realizar,
-	        comentarios,
-	        fecha_ini_accion,
-	        fecha_ter_accion,
-	        id_estado,
-            fecha_ingreso_accion
-        )
-        values(
-            p_num_cuenta,
-            'Direccion de domicilio: ' || coalesce(p_direccion, 'No especificado'),
-            'Alta de domicilio',
-            'No se informa la direccion',
-            CURRENT_DATE,
-            NULL,
-            --aqui deberia ir el id_estado correspondiente a 'Error'
-            --fecha ingreso no seria redundante?
-        );
-    --prueba 4
-    else if p_id_contrato is null then
-        insert into accion(
-            destinatario_accion,
-	        parametros,
-	        accion_realizar,
-	        comentarios,
-	        fecha_ini_accion,
-	        fecha_ter_accion,
-	        id_estado,
-            fecha_ingreso_accion
-        )
-        values(
-            p_num_cuenta,
-            'Id_contrato: ' || coalesce(p_id_contrato, 'No especificado'),
-            'Alta de domicilio',
-            'No se informa el contrato',
-            CURRENT_DATE,
-            NULL,
-            --aqui deberia ir el id_estado correspondiente a 'Error'
-            --fecha ingreso no seria redundante?
-        );
-    end if;
+    case
+        when p_direccion is null or trim(p_direccion) = '' then
+            insert into accion(
+                destinatario_accion,
+	            parametros,
+	            accion_realizar,
+	            comentarios,
+	            fecha_ini_accion,
+	            fecha_ter_accion,
+	            id_estado,
+                fecha_ingreso_accion
+            )
+            values(
+                p_num_cuenta,
+                'Direccion de domicilio: ' || coalesce(p_direccion, 'No especificado'),
+                'Alta de domicilio',
+                'No se informa la direccion',
+                CURRENT_DATE,
+                NULL,
+                --aqui deberia ir el id_estado correspondiente a 'Error'
+                --fecha ingreso no seria redundante?
+            );
+        --prueba 4
+        when p_id_contrato is null then
+            insert into accion(
+                destinatario_accion,
+	            parametros,
+	            accion_realizar,
+	            comentarios,
+	            fecha_ini_accion,
+	            fecha_ter_accion,
+	            id_estado,
+                fecha_ingreso_accion
+            )
+            values(
+                p_num_cuenta,
+                'Id_contrato: ' || coalesce(p_id_contrato, 'No especificado'),
+                'Alta de domicilio',
+                'No se informa el contrato',
+                CURRENT_DATE,
+                NULL,
+                --aqui deberia ir el id_estado correspondiente a 'Error'
+                --fecha ingreso no seria redundante?
+            );
+    end case;
 
     insert into domicilio(
         num_cuenta,
