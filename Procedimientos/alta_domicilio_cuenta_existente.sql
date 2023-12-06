5ead79f5d3786812c496601810dbaa219c0ad12a
garaneda <garaneda@utem.cl>
2023-12-06 17:05:29 -0300

    los datos ahora se guardan en un arreglo

--- Procedimientos/alta_domicilio_cuenta_existente.sql
+++ Procedimientos/alta_domicilio_cuenta_existente.sql
@@ -8,16 +8,33 @@ DECLARE
     v_direccion varchar;
     v_territorio varchar;
     v_plan varchar;
-    v_id_contrato integer;
+    v_id_contrato varchar;
     v_tipo_domicilio varchar;
-    v_num_cuenta integer;
-    v_id_sucursal integer;
-    v_id_vendedor integer;
+    v_num_cuenta varchar;
+    v_id_sucursal varchar;
+    v_id_vendedor varchar;
 	
+	datos varchar[];
+	prefijos varchar[] := ARRAY['DIR','TRR','PLN','IDCTR','TDDOM','IDACC','IDSUC','IDVEND'];
 	error_detectado bool := false;
 BEGIN
-	v_direccion := obtener_dato(Parametros,'DIR',0);
-	RAISE NOTICE 'direccion obtenida: %', v_direccion;
-    
+
+	FOR i IN 1..8 LOOP
+		datos[i]=obtener_dato(Parametros,prefijos[i],0);
+		
+		IF datos[i] = 'NO EXISTE' THEN
+			error_detectado := true;
+		END IF;
+		
+		--RAISE NOTICE 'dato[%] = %',i,datos[i];
+		--RAISE NOTICE 'error detectado = %',error_detectado;
+	END LOOP;
+	
+	IF error_detectado <> true THEN
+		RAISE NOTICE 'NO SE DETECTARON ERRORES :)';
+	ELSE
+		RAISE NOTICE 'HUBO UN ERROR :(';
+	END IF;
+	
 END; $$
 LANGUAGE plpgsql;
