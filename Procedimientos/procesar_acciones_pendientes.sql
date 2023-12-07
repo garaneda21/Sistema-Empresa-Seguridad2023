677750fd29892510ee8e54de8312791c2acd46a9
garaneda21 <151650052+garaneda21@users.noreply.github.com>
2023-12-06 22:15:58 -0300

    Update procesar_acciones_pendientes.sql

--- Procedimientos/procesar_acciones_pendientes.sql
+++ Procedimientos/procesar_acciones_pendientes.sql
@@ -4,30 +4,18 @@ DECLARE
 	DECLARE cursor_accion CURSOR FOR
     SELECT *
     FROM accion;
-
 BEGIN
 	FOR reg IN cursor_accion LOOP
 		IF reg.id_estado <> 0 THEN
-			RAISE NOTICE 'accion[%] procesada o con error', reg.id_accion;
 		ELSE
 			CASE reg.id_accion_realizar
 			WHEN 0 THEN 
-				-- accion 0
-				RAISE NOTICE 'accion[%] ejecuta alta cuenta existente', reg.id_accion;
+				UPDATE accion SET fecha_ini_accion = now() WHERE id_accion = reg.id_accion;
 				CALL alta_domicilio_cuenta_existente(reg.parametros_concat,reg.id_accion);
 			WHEN 1 THEN 
-				-- accion 1
-				RAISE NOTICE 'accion[%] ejecuta alta cuenta nueva', reg.id_accion;
 				CALL alta_domicilio_cuenta_nueva(reg.parametros_concat,reg.id_accion);
 			WHEN 2 THEN 
-				-- accion 2
-				RAISE NOTICE 'accion[%] ejecuta alta nuevo cliente', reg.id_accion;
-				CALL alta_domicilio_nuevocliente(reg.parametros_concat,reg.id_accion);
 			WHEN 3 THEN
-				-- accion 3
-				RAISE NOTICE 'accion[%] ejecuta alta de un articulo', reg.id_accion;
-			ELSE 
-				--else_result 
 			END CASE;
 		END IF;
 	END LOOP;
